4e53508ad4872a39a93b08e1f8c427bcc8289c11
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-06-23 11:28:49 +0530

    Completed Exercise 9: Spring Boot Library Management Application

---
+++ DN-JavaFSE-Solutions/WEEK-2/Spring_Core_Maven/LibraryManagementBoot/src/main/java/com/library/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.library.controller;
+
+import com.library.model.Book;
+import com.library.repository.BookRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    // Get all books
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    // Add a new book
+    @PostMapping
+    public Book addBook(@RequestBody Book book) {
+        return bookRepository.save(book);
+    }
+}
