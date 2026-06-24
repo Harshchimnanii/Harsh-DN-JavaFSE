490a992a02d088bcdfd48b6f490b275897223c04
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-06-24 09:36:20 +0530

    Completed Spring Data JPA Query Methods for Country and Stock

--- DN-JavaFSE-Solutions/WEEK-2/1. spring-data-jpa-handson/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
+++ DN-JavaFSE-Solutions/WEEK-2/1. spring-data-jpa-handson/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -1,8 +1,9 @@
 package com.cognizant.ormlearn;
 
 import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+import com.cognizant.ormlearn.repository.StockRepository;
 import com.cognizant.ormlearn.service.CountryService;
-import com.cognizant.ormlearn.service.exception.CountryNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -20,67 +21,46 @@ public class OrmLearnApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
         countryService = context.getBean(CountryService.class);
+        CountryRepository countryRepository = context.getBean(CountryRepository.class);
+        StockRepository stockRepository = context.getBean(StockRepository.class);
 
         LOGGER.info("Inside main");
 
         try {
-            // Hands-on 1: Get All Countries
-            testGetAllCountries();
+            // ==========================================
+            // MODULE 1: CRUD OPERATIONS (Purana Code Safe Hai)
+            // (Commented out so they don't insert duplicates on every run)
+            // ==========================================
+            // testGetAllCountries();
+            // testFindCountryByCode();
+            // testAddCountry();
+            // testUpdateCountry();
+            // testDeleteCountry();
 
-            // Hands-on 6: Find Country by Code
-            testFindCountryByCode();
 
-            // Hands-on 7: Add a new Country
-            testAddCountry();
 
-            // Hands-on 8: Update Country
-            testUpdateCountry();
+            // --- Hands-on 1 Tests (Country Query Methods) ---
+            LOGGER.info("1. Countries containing 'ou' in Ascending order:");
+            List<Country> ouCountries = countryRepository.findByNameContainingOrderByNameAsc("ou");
+            LOGGER.debug("Countries: {}", ouCountries);
 
-            // Hands-on 9: Delete Country
-            testDeleteCountry();
+            LOGGER.info("2. Countries starting with 'Z':");
+            List<Country> zCountries = countryRepository.findByNameStartingWith("Z");
+            LOGGER.debug("Countries: {}", zCountries);
+
+            // --- Hands-on 2 Tests (Stock Query Methods) ---
+            LOGGER.info("3. Google stocks where close price > 1250:");
+            stockRepository.findByCodeAndCloseGreaterThan("GOOGL", 1250.0).forEach(stock -> LOGGER.debug(stock.toString()));
+
+            LOGGER.info("4. Top 3 FB stocks by Volume:");
+            stockRepository.findTop3ByCodeOrderByVolumeDesc("FB").forEach(stock -> LOGGER.debug(stock.toString()));
+
+            LOGGER.info("5. Lowest 3 NFLX stocks by Close price:");
+            stockRepository.findTop3ByCodeOrderByCloseAsc("NFLX").forEach(stock -> LOGGER.debug(stock.toString()));
 
         } catch (Exception e) {
             LOGGER.error("Error occurred: ", e);
         }
     }
 
-    private static void testGetAllCountries() {
-        LOGGER.info("Start: testGetAllCountries");
-        List<Country> countries = countryService.getAllCountries();
-        LOGGER.debug("countries={}", countries);
-        LOGGER.info("End: testGetAllCountries");
-    }
-
-    private static void testFindCountryByCode() throws CountryNotFoundException {
-        LOGGER.info("Start: testFindCountryByCode");
-        Country country = countryService.findCountryByCode("IN");
-        LOGGER.debug("Country:{}", country);
-        LOGGER.info("End: testFindCountryByCode");
-    }
-
-    private static void testAddCountry() throws CountryNotFoundException {
-        LOGGER.info("Start: testAddCountry");
-        Country newCountry = new Country("SL", "Sri Lanka");
-        countryService.addCountry(newCountry);
-
-        Country fetchedCountry = countryService.findCountryByCode("SL");
-        LOGGER.debug("Added Country:{}", fetchedCountry);
-        LOGGER.info("End: testAddCountry");
-    }
-
-    private static void testUpdateCountry() throws CountryNotFoundException {
-        LOGGER.info("Start: testUpdateCountry");
-        countryService.updateCountry("SL", "Sri Lanka Updated");
-
-        Country fetchedCountry = countryService.findCountryByCode("SL");
-        LOGGER.debug("Updated Country:{}", fetchedCountry);
-        LOGGER.info("End: testUpdateCountry");
-    }
-
-    private static void testDeleteCountry() {
-        LOGGER.info("Start: testDeleteCountry");
-        countryService.deleteCountry("SL");
-        LOGGER.debug("Deleted Country with code SL");
-        LOGGER.info("End: testDeleteCountry");
-    }
 }
