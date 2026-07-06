52bc481ce7e3f360ca0913bf3ba8630a00078152
Harsh Chimnani <chimnaniharsh4@gmail.com>
2026-07-06 12:37:19 +0530

    day 1 first ques done

---
+++ DAY_1/MinMax.java
@@ -0,0 +1,25 @@
+class FindMinMax{
+    static int[] maxOrmin(int a[]){
+        int min = a[0], max = a[0];
+        for (int i = 0; i < a.length; i++) {
+            if (min > a[i]) {
+                min = a[i];
+            }
+            if(max<a[i]) {
+                max = a[i];
+                // min = a[i];
+            }
+        }
+
+        return new int[] {max , min};
+    }
+}
+public class MinMax {
+
+    public static void main(String[] args) {
+        int arr[] = { 2 , 32 , 33, 44 ,5 };
+        FindMinMax minmax = new FindMinMax();
+        int [] result = minmax.maxOrmin(arr);
+        System.out.println(result[0] +" "+ result[1]);
+    }
+}
